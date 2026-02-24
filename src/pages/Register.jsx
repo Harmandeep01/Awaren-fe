b5f9824ea2d7fc241a0949bc582e8e8d893062b1
Harmandeep01 <sharmandeep@gmail.com>
2026-02-24 18:46:21 +0530

    added api

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-
+import api from '../config/api'
 export default function Register() {
   const navigate = useNavigate();
   // Sync theme with document root
